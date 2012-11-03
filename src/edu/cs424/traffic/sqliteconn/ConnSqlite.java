40a2c4289ac61c4eacdcc94137930f7083bb655e
kevin varkey <kevingeorge@gmail.com>
2012-11-02 19:32:17 -0500

    conn sql

--- src/edu/cs424/traffic/sqliteconn/ConnSqlite.java
+++ src/edu/cs424/traffic/sqliteconn/ConnSqlite.java
@@ -7,13 +7,14 @@ import java.util.Map.Entry;
 
 import com.modestmaps.geo.Location;
 
+import edu.cs424.traffic.components.BarGraph.Type;
 import edu.cs424.traffic.components.MapPanel;
 import edu.cs424.traffic.map.dataset.DataPoint;
 
 public class ConnSqlite {
 
 	static Connection conn;
-	
+
 	static {
 		String sqliteDB = "jdbc:sqlite:../RearView2.sqlite";
 		try {
@@ -27,24 +28,38 @@ public class ConnSqlite {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String getCrashByState(FilterData filterData) {
 
 		return appendFilters(filterData, "SELECT latitude, longitude, istatenum FROM f2");
 	}
-	
-	public static String getCrashByYear(FilterData filterData) {
 
-		return appendFilters(filterData, "SELECT latitude, longitude, Year, iaccmon, dayofweek FROM f2");
+	public static HashMap<String, ArrayList<DataPoint>> getCrashData( FilterData filterData,Type type) 
+	{
+		String query;
+		if ( type == type.Year )
+		{
+			query =  appendFilters(filterData, "SELECT UID, latitude, longitude, Year FROM f2");
+		}
+		else if (type == type.Month )
+		{
+			query =  appendFilters(filterData, "SELECT UID, latitude, longitude, iaccmon FROM f2");
+		}
+		else
+		{
+			query =  appendFilters(filterData, "SELECT UID, latitude, longitude, dayofweek FROM f2");
+		}
+
+		return executeFilterQuery(query, type);
 	}
-	
-	static String appendFilters(FilterData filterData, String query) {
-		
+
+	private static String appendFilters(FilterData filterData, String query) {
+
 		HashMap<String, ArrayList<Integer>> values = filterData.getSelectedValues();
-		
+
 		StringBuffer append = new StringBuffer("");
 		boolean first1 = true;
-		
+
 		if(! values.isEmpty()) {
 			append.append(" where ");
 
@@ -53,15 +68,15 @@ public class ConnSqlite {
 				boolean first2 = true;
 				if( !first1 )
 					append.append(" and ");
-				
+
 				first1 = false;
 				append.append(filter.getKey());
 				append.append(" in ( ");				
-				
+
 				for(Integer num : filter.getValue()) {				
 					if ( !first2 )
 						append.append(", ");
-					
+
 					first2 = false;
 					append.append(num);				
 				}
@@ -70,41 +85,67 @@ public class ConnSqlite {
 		}
 		append.append(";");
 		query += append.toString();
-		
+
 		System.out.println("Query: " + query);
-		
+
 		return query;
 	}
-	
-	public static String getLocations(FilterData filterData) {
-		
-		return appendFilters(filterData, "SELECT latitude, longitude FROM f2");
-	}
-	
-	public static ArrayList<DataPoint> executeQuery(String query) {
+
+
+	private static HashMap<String, ArrayList<DataPoint>> executeFilterQuery(String query,Type type) 
+	{
 		Statement stat;
 		ResultSet res;
-		ArrayList<DataPoint> points = new ArrayList<DataPoint>();
-		
-		try {
+		HashMap<String, ArrayList<DataPoint>> result = new HashMap<String, ArrayList<DataPoint>>();
+
+		try 
+		{
 			stat = conn.createStatement();
 			res = stat.executeQuery(query);
-			
-			while( res.next() ){
-				DataPoint dp = new DataPoint(res.getDouble("latitude"), res.getDouble("longitude"), 1, false);
-				points.add(dp);
+
+			while( res.next() )
+			{
+				String key;
+				Location loc = new Location(res.getFloat("latitude"), res.getFloat("longitude"));
+				
+				if ( type == type.Year )
+				{
+					key = res.getString("Year");
+				}
+				else if (type == type.Month )
+				{
+					key = res.getString("iaccmon");
+				}
+				else
+				{
+					key = res.getString("dayofweek");
+				}
+				
+				if( !result.containsKey(key) )
+				{
+					ArrayList<DataPoint> temp = new ArrayList<DataPoint>();
+					result.put(key, temp);
+				}
+				
+				DataPoint dp = new DataPoint(res.getString("UID"),loc, key);
+				result.get(key).add(dp);				
 			}
-			
-		} catch (SQLException e) {
+
+		} 
+		catch (SQLException e) 
+		{
 			e.printStackTrace();
 		}
-		return points;
+
+		return result;
 	}
-	
+
+
+
 	public static void cleanup() {
 		try {
 			conn.close();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
