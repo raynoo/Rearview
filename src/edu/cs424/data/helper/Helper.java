a05e46b1045a1840661bcf313f0e9934e1ea5030
payas89 <payas.tikotekar@gmail.com>
2012-11-05 04:21:19 -0600

    Helper Class

--- src/edu/cs424/data/helper/Helper.java
+++ src/edu/cs424/data/helper/Helper.java
@@ -10,11 +10,18 @@ public class Helper {
 		
 		
 		ArrayList<String> s11 = new ArrayList<String>();
-		s11.add("Driver Age = 1");
+		s11.add("Driver Age = < 16");
 		String s12 = "Somethings";
-		Findings f1 = new Findings(s11, s12, "", 0.0f, 0.0f, 0);
+		Findings f1 = new Findings(s11, s12, "", 0.0f, 0.0f, 0);	
 		sending.add(f1);
 		
+		ArrayList<String> s21 = new ArrayList<String>();
+		s21.add("Body Type = Motorcycle");
+		s21.add("Driver Age = 51 - 75");
+		s21.add("Body Type = > 75");
+		String s22 = "Somethings2";
+		Findings f2 = new Findings(s21, s22, "", 0.0f, 0.0f, 0);	
+		sending.add(f2);
 		
 		return sending;
 	}
